ca8ddb63abaf473b04113d40ad8862fcb3337b16
Doro97 <dorothycheruiyot97@gmail.com>
2021-09-23 22:29:01 +0300

    bash script to execute mysqlconnect script

---
+++ airflow-engine/airflow.sh
@@ -0,0 +1,19 @@
+INIT_FILE=.airflowinitialized
+if [ ! -f «$INIT_FILE» ]; then
+    # Create all Airflow configuration files
+    airflow initdb
+    rm /root/airflow/airflow.db
+    
+  
+    # Setup the DB
+    python mysqlconnect.py
+    airflow initdb
+    
+    # This configuration is done only the first time
+    touch «$INIT_FILE»
+fi
+
+# Run the Airflow webserver and scheduler
+airflow scheduler &
+airflow webserver &
+wait 
