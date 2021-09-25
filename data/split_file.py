7be480557a9a2b6a0eeff77398e836e3bc33fd2e
Doro97 <dorothycheruiyot97@gmail.com>
2021-09-25 23:00:13 +0300

    python script to split the large files into fixed sized files

--- data/split_file.py
+++ data/split_file.py
@@ -1,4 +1,4 @@
-def split_file(file_name, lines_per_file=1000):
+def split_file(file_name, lines_per_file=100000):
     # Open large file to be read in UTF-8
     with open(file_name, 'r') as rf:
         # Read all lines in file
@@ -10,7 +10,7 @@ def split_file(file_name, lines_per_file=1000):
         # For x from 0 to length of lines read stepping by number of lines that will be written in each file
         for x in range(0, len(lines), lines_per_file):
             # Open new "split" file for writing in UTF-8
-            with open( 'data' + '-' + str(file_no) + '.txt', 'w') as wf:
+            with open( 'sample' + '-' + str(file_no) + '.txt', 'w') as wf:
                 # Write lines
                 wf.writelines(lines[x:x+lines_per_file])
                 # Update the written lines count
@@ -19,5 +19,5 @@ def split_file(file_name, lines_per_file=1000):
                 file_no+=1
         print(str(wlines_count) + " LINES WRITTEN IN " + str(file_no) + " FILES.")
 
-# Split I80_davis.txt into files containing 1000 lines
-split_file(r'C:\Users\user.DESKTOP-OMQ89VA\Desktop\datawarehoue\datawarehouse_techstack\data\I80_davis.txt',1000)
+# Split I80_davis.txt into files containing 100000 lines
+split_file(r'C:\Users\user.DESKTOP-OMQ89VA\Desktop\datawarehoue\datawarehouse_techstack\data\I80.txt',100000)
